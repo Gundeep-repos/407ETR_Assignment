472ecbf9e5cfae0d7f33e1387f6f09619d7dada9
Gundeep Singh <gundeep5830@gmail.com>
2021-07-04 17:06:43 -0400

    First commit

---
+++ 407ETR_Gundeep_Singh_assignment/src/ServiceClass.java
@@ -0,0 +1,61 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.json.simple.JSONObject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class ServiceClass
+ */
+@WebServlet("/ServiceClass")
+public class ServiceClass extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServiceClass() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		JSONObject returnedObject = null;
+		
+		
+		
+		String sPoint = request.getParameter("start");
+		String ePoint = request.getParameter("end");
+
+		HelperClass helperObj = new HelperClass();
+		returnedObject = helperObj.calculate(sPoint.trim(), ePoint.trim());
+		out.print(returnedObject.toJSONString());
+			
+
+		
+		
+}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
